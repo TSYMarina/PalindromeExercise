c1535034136ea4f46e800d6d7149d0a74e167858
TSYMarina <marinatsy16@gmail.com>
2022-03-07 13:05:04 -0700

    first commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+
+            var revWord = "";
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                revWord += word[i];
+            }
+            if (word.ToLower() == revWord.ToLower())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+            // attempt at refactoring...:
+
+            //var reversed = word.Reverse().ToString().ToLower();
+            //var answer = word == reversed ? true : false;
+            //return answer;
+
+
+
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,26 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("civic", true)]
+        [InlineData("word", false)]
+        [InlineData("Madam", true)]
+        [InlineData("radar", true)]
+        [InlineData("backwards", false)]
+
+        public void Test1(string word, bool expected)
         {
+            //Arrange:
+            var tester = new WordSmith();
+            //Act:
+            var actual = tester.IsAPalindrome(word);
+            //Assert:
+            Assert.Equal(expected, actual);
 
         }
     }
